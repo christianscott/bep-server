63a815dc336a7ad276da664f620b1356d80fadee
Christian Scott <christian.s@canva.com>
2023-02-17 10:20:04 +1100

    parse bazel build events

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 extern crate futures;
 extern crate grpc;
+extern crate protobuf;
 extern crate tls_api_stub;
 
+use publish_build_event_proto_rs::build_event_stream;
+use publish_build_event_proto_rs::build_events;
 use publish_build_event_proto_rs::{
     Empty, PublishBuildEvent, PublishBuildEventServer, PublishBuildToolEventStreamRequest,
     PublishBuildToolEventStreamResponse, PublishLifecycleEventRequest,
@@ -13,33 +16,54 @@ use std::thread;
 
 struct PublishBuildEventImpl;
 
+impl PublishBuildEventImpl {
+    fn handle_build_event(&self, event: build_events::BuildEvent) -> () {
+        match event.event {
+            Some(build_events::BuildEvent_oneof_event::bazel_event(any)) => {
+                if any.type_url == "type.googleapis.com/build_event_stream.BuildEvent" {
+                    let msg =
+                        protobuf::parse_from_bytes::<build_event_stream::BuildEvent>(&any.value)
+                            .expect("could not parse BuildEvent");
+                    println!("{:#?}", msg);
+                }
+            }
+            _ => {}
+        }
+    }
+}
+
 impl PublishBuildEvent for PublishBuildEventImpl {
     fn publish_lifecycle_event(
         &self,
-        _o: ::grpc::RequestOptions,
+        _o: grpc::RequestOptions,
         _req: PublishLifecycleEventRequest,
-    ) -> ::grpc::SingleResponse<Empty> {
+    ) -> grpc::SingleResponse<Empty> {
         println!("publish_lifecycle_event");
         grpc::SingleResponse::completed(Empty::new())
     }
 
     fn publish_build_tool_event_stream(
         &self,
-        _o: ::grpc::RequestOptions,
-        mut req: ::grpc::StreamingRequest<PublishBuildToolEventStreamRequest>,
-    ) -> ::grpc::StreamingResponse<PublishBuildToolEventStreamResponse> {
+        _o: grpc::RequestOptions,
+        mut req: grpc::StreamingRequest<PublishBuildToolEventStreamRequest>,
+    ) -> grpc::StreamingResponse<PublishBuildToolEventStreamResponse> {
         println!("publish_build_tool_event_stream");
 
         let mut r = Vec::new();
         loop {
             match req.0.poll() {
                 Ok(futures::Async::Ready(None)) => {
+                    // stream closed
                     break;
                 }
                 Ok(futures::Async::Ready(Some(req))) => {
-                    println!("{:?}", req);
                     let mut res = PublishBuildToolEventStreamResponse::new();
-                    res.set_sequence_number(req.get_ordered_build_event().sequence_number);
+                    let ordered_build_event = req.get_ordered_build_event();
+
+                    self.handle_build_event(ordered_build_event.event.clone().unwrap());
+
+                    res.set_sequence_number(ordered_build_event.sequence_number);
+                    res.set_stream_id(ordered_build_event.stream_id.clone().unwrap());
                     r.push(res)
                 }
                 _ => {}
@@ -61,7 +85,7 @@ fn main() {
     server.http.set_cpu_pool_threads(4);
     let server = server.build().expect("server");
     let port = server.local_addr().port().unwrap();
-    println!("greeter server started on port {port}");
+    println!("BEP server started on port {port}");
 
     loop {
         thread::park();
